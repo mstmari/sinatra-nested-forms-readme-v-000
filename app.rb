04886f28e89f53f4207ad9be42627b5d4dc966ba
Michael St. Marie <michael.landrum.st.marie@gmail.com>
2018-04-18 01:15:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
- 
+
+  get '/' do
+    erb :index
+  end
+
+ post '/student' do
+   puts params
+     @student = Student.new(params[:student])
+
+     params[:student][:courses].each do |details|
+       Course.new(details)
+     end
+
+     @courses = Course.all
+
+     erb :student
+   end
+
+
 end
